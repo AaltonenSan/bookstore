d11484739f486081f0f459928cb05a29ad745970
AaltonenSan <santtu.36@gmail.com>
2022-10-11 17:13:36 +0300

    C6.1 Added Spring Security for in-memory users

--- bookstore/src/main/java/hh/swd02/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/swd02/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -55,6 +56,7 @@ private CategoryRepository crepository;
 		return "addbook";
 	}
 	
+	@PreAuthorize(value="hasRole('ADMIN')")
 	@GetMapping("/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
@@ -66,6 +68,11 @@ private CategoryRepository crepository;
 		model.addAttribute("book", repository.findById(bookId));
 		model.addAttribute("categories", crepository.findAll());
 		return "editbook";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 	
 	@PostMapping("/save")
